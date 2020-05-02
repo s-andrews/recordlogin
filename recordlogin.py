a5e55657fb0651d74f19a247436e22138903278a
Simon Andrews <simon@proteo.me.uk>
2020-05-02 16:04:16 +0000

    Got the main display and input working

--- recordlogin.py
+++ recordlogin.py
@@ -8,8 +8,16 @@ def curses_main(screen):
 	name = ""
 	while(True):
 		new_char = draw_screen(screen,name)
-		if new_char=="q":
+		if new_char=="\n" and len(name)>0:
 			break;
+
+
+		elif new_char=="KEY_BACKSPACE" and len(name)>0:
+			name=name[:-1]
+
+		elif new_char.startswith("KEY"):
+			continue
+
 		name = name+new_char
 
 
@@ -19,18 +27,38 @@ def draw_screen(screen, name):
 	screen.clear()
 	screen.refresh()
 
+	height, width = screen.getmaxyx()
+
 	curses.start_color()
 	curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLUE)
 	curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
+	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
 
 	screen.bkgd(' ',curses.color_pair(1))
-	screen.addstr(0,0,"Hello, curses world", curses.color_pair(2) | curses.A_BOLD)
+
+	main_title = "BABRAHAM BIOINFORMATICS TEST SERVER"
+	screen.addstr(1,get_centre_offset(main_title, width),main_title, curses.color_pair(2) | curses.A_BOLD)
+
+	congrats_text = "Congratulations, you logged in successfully"
+	screen.addstr(4,get_centre_offset(congrats_text, width),congrats_text, curses.color_pair(2))
+
+	name_please_text = "Please type your name so we know who you are"
+	screen.addstr(5,get_centre_offset(name_please_text, width),name_please_text, curses.color_pair(2))
+
+	user_name = name.ljust(50,".")
+	screen.addstr(10,get_centre_offset(user_name, width),user_name, curses.color_pair(3))
+
+	# Put the cursor where they're going to have to type
 
 	screen.refresh()
 
 	x = screen.getkey()
 
-	return(x)
+	return x
+
+
+def get_centre_offset(text,width):
+	return int(width/2 - len(text)/2)
 
 
 def main():
